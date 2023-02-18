013dd92dc572fc68be6aad057b4be6bf9f664144
Lee-chaerin <5543705@naver.com>
2023-02-18 21:54:29 +0900

    라우터 적용 및 주요페이지 주소 연결

--- deptsite/src/App.js
+++ deptsite/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Route, Routes } from 'react-router-dom';
+
 import MainPage from './pages/MainPage';
 import IntroducePage from './pages/IntroducePage';
 import ProjectPage from './pages/ProjectPage';
@@ -9,15 +11,16 @@ import QnAPage from './pages/QnAPage';
 import './App.css';
 
 
+
 const App = () => {
   return (
-    <>
-      <MainPage/>
-      <IntroducePage/>
-      <ProjectPage/>
-      <StudyPage/>
-      <QnAPage/>
-    </>
+    <Routes>
+      <Route path="/" element={<MainPage/>}/>
+      <Route path="/introduce" element={<IntroducePage/>}/>
+      <Route path="/project" element={<ProjectPage/>}/>
+      <Route path="/study" element={<StudyPage/>}/>
+      <Route path="/qna" element={<QnAPage/>}/>
+    </Routes>
   );
 };
 
