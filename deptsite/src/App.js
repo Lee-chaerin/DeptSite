f893c10eca3d6a9db62af06f4910b711bcb9d48a
Lee-chaerin <5543705@naver.com>
2023-02-18 21:59:05 +0900

    header, footer 생성

--- deptsite/src/App.js
+++ deptsite/src/App.js
@@ -8,19 +8,25 @@ import ProjectPage from './pages/ProjectPage';
 import StudyPage from './pages/StudyPage';
 import QnAPage from './pages/QnAPage';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
 import './App.css';
-
-
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<MainPage/>}/>
-      <Route path="/introduce" element={<IntroducePage/>}/>
-      <Route path="/project" element={<ProjectPage/>}/>
-      <Route path="/study" element={<StudyPage/>}/>
-      <Route path="/qna" element={<QnAPage/>}/>
-    </Routes>
+    <>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<MainPage/>}/>
+        <Route path="/introduce" element={<IntroducePage/>}/>
+        <Route path="/project" element={<ProjectPage/>}/>
+        <Route path="/study" element={<StudyPage/>}/>
+        <Route path="/qna" element={<QnAPage/>}/>
+      </Routes>
+      <Footer/>
+    </>
   );
 };
 
---
+++ deptsite/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <hr/>
+      <h1>푸터</h1>
+    </>
+  );
+};
+
+export default Footer;
