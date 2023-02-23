1e187081751725d291cb0bb196e63d3ebfc34d5e
Lee-chaerin <5543705@naver.com>
2023-02-24 02:23:27 +0900

    header 뼈대 제작 및 css 작업

--- deptsite/src/components/Header.jsx
+++ deptsite/src/components/Header.jsx
@@ -2,10 +2,34 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <>
-      <h1>헤더</h1>
-      <hr/>
-    </>
+    <div id="header">
+      <span>
+        <img src='ptuLogo.png' alt='ptuLogo'/>
+        <span>
+          <h1>융합소프트웨어학과</h1>
+          <h4>Department of Convergence Software</h4>
+        </span>
+      </span>
+
+      <span>
+        <ul>
+          <li>INTRODUCE
+            <ul>
+              <li>학과 소개</li>
+              <li>전공교수 소개</li>
+              <li>임원 소개</li>
+              <li>학과 공지</li>
+              <li>학과 갤러리</li>
+            </ul>
+          </li>
+          <li>STUDY</li>
+          <li>PROJECT</li>
+          <li>Q&A</li>
+          <li>로그인</li>
+        </ul>
+      </span>
+
+    </div>
   );
 };
 
