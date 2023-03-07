5387c76906cfeac1b78daf9a678c372e5fb86c89
Lee-chaerin <5543705@naver.com>
2023-03-07 12:07:23 +0900

    IntroducePage 뼈대 제작

--- deptsite/src/pages/IntroducePage.jsx
+++ deptsite/src/pages/IntroducePage.jsx
@@ -7,6 +7,16 @@ const IntroducePage = () => {
         <h1>INTRODUCE</h1>
       </div>
       <div className='main'>
+        <div className='left'>
+          <ul>
+            <li>학과 소개</li>
+            <li>전공교수 소개</li>
+            <li>임원 소개</li>
+            <li>학과 공지</li>
+            <li>학과 갤러리</li>
+          </ul>
+        </div>
+        <div className='right'></div>
 
       </div>
     </div>
