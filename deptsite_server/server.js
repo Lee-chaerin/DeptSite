68051887da2b979dcc6805248695cf4d02c7ac2c
Lee-chaerin <5543705@naver.com>
2023-03-21 10:37:33 +0900

    데이터베이스 불러오기

--- deptsite_server/server.js
+++ deptsite_server/server.js
@@ -1,13 +1,34 @@
 const express = require('express');
-const path = require('path');
+const cors = require('cors');
+const mysql = require('mysql');
 const app = express();
+const path = require('path');
 
-app.listen(8080, function () {
+
+const db = mysql.createPool({
+  hoet: "localhost",
+  user: "root",
+  password: "cofls0408",
+  database: "deptsite",
+});
+
+app.listen(8000, function () {
   console.log('listening on 8080')
 }); 
 
+app.get('/api/db', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  const sqlQuery = "SELECT * FROM USERS";
+
+  db.query(sqlQuery, (err, result) => {
+    res.send(result);
+  });
+});
+
+//app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'deptsite/build')));
 
-app.get('/', function(요청, 응답) {
-  응답.sendFile(path.join(__dirname, 'deptsite/build/index.html'));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'deptsite/build/index.html'));
 })
