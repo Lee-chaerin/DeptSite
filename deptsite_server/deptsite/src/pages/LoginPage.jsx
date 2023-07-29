9266666b052ad5fb623aae71504f28193ef4a3f2
Lee-chaerin <5543705@naver.com>
2023-07-30 03:02:14 +0900

    LoginPage 간단한 로그인 기능 제작

--- deptsite_server/deptsite/src/pages/LoginPage.jsx
+++ deptsite_server/deptsite/src/pages/LoginPage.jsx
@@ -1,6 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginPage = () => {
+  const[user, setUser] = useState({
+    student_id: 0,
+    password: ''
+  });
+
+  const setChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+    console.log(user);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`http://localhost:8000/api/users?id=${user.student_id}`, {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+      });
+      const data = await res.json();
+      console.log('서버 응답:', data);
+
+      if(data.length === 0) {
+        alert("존재하지 않는 아이디입니다.")
+      } else if(data[0].password === user.password) {
+        alert("로그인 성공")
+      } else {
+        alert("비밀번호가 다릅니다.")
+      }
+      
+    } catch (error) {
+      console.error('API 호출 오류:', error);
+    }
+  };
+
   return (
     <div id='loginPage'>
       <div className='main pageTop'>
@@ -9,19 +45,19 @@ const LoginPage = () => {
 
           <h1>로그인</h1>
 
-          <div>
+          <form onSubmit={handleSubmit}>
             <span>
               <div style={{marginBottom: "10px"}}>
                 <p>아이디</p>
-                <input></input>
+                <input type='text' name='student_id' maxLength='10' required onChange={setChange}/>
               </div>
               <div>
                 <p>비밀번호</p>
-                <input></input>
+                <input type='password' name='password' maxLength='15' required onChange={setChange}/>
               </div>
             </span>
-            <span><button>로그인</button></span>
-          </div>
+            <span><button type='submit'>로그인</button></span>
+          </form>
 
           <div style={{marginTop: "50px"}}>
             <button style={{marginRight: "25px"}}>아이디/비밀번호 찾기</button>
